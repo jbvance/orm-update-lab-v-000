f6a345ee49ef9ebfa684e506c6eeaafc8e288b17
Jason Vance <jbvance@gmail.com>
2017-12-05 18:31:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ attr_reader :id
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE students IF NOT EXISTS (
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
