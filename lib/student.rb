4296b2d6ddab1f9032d32a10a06228e61c8a8bcd
Jason Vance <jbvance@gmail.com>
2017-12-05 18:30:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,8 +7,22 @@ attr_reader :id
 
   def initialize(id=nil, name, grade)
     @id, @name, @grade = id, name grade
-
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students IF NOT EXISTS (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute (sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE  IF EXISTS students"
+    DB[:conn].execute (sql)
+  end
 
 end
