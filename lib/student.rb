67c36821cc691de8b09585e8a599f7849caa1b15
Jason Vance <jbvance@gmail.com>
2017-12-05 18:35:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ attr_reader :id
       INSERT INTO students (name, grade) values (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
   end
 
