fbf53322ec9d9ee662a81e633125250b0f657617
Jason Vance <jbvance@gmail.com>
2017-12-05 18:30:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 attr_reader :id
 
   def initialize(id=nil, name, grade)
-    @id, @name, @grade = id, name grade
+    @id, @name, @grade = id, name, grade
   end
 
   def self.create_table
